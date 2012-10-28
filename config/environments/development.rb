b08ef1e43ade10ec48f86673c5070039ba005651
ark5 <andrewkshim92@gmail.com>
2012-10-27 22:02:24 -0400

    confirmation email in progress

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,22 @@ Nubhub::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.devliery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'localhost', :port => "1025"   }
+
+ #Added per active admin install instructions
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "mail.google.com",
+    :user_name => "noteflote@gmail.com",
+    :password => "dukefluke",
+    :authentication => :plain,
+    :enable_starttls_auto => true 
+  }
 end
